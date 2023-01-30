c8f099db6d49396502ba2dfcd6372165d282df3a
nate Hagen <nates50checy@gmail.com>
2023-01-30 10:22:29 -0500

    external javascript

---
+++ Milestone1/index.js
@@ -0,0 +1,6 @@
+function openLogin() {
+    document.getElementById("loginBox").style.display = "block";
+}
+function closeLogin() {
+    document.getElementById("loginBox").style.display = "none";
+}
