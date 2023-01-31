72d7c678fef58a5be786c9541feec9c0bdc57bfe
nate Hagen <nates50checy@gmail.com>
2023-01-31 13:37:55 -0500

    nvm not needed yet

--- Milestone2/README.md
+++
@@ -1,1 +0,0 @@
-This is where we will work oine milestone one, once doen copy files into milestone 2 for next steps.
--- Milestone2/index.js
+++
@@ -1,6 +0,0 @@
-function openLogin() {
-    document.getElementById("loginBox").style.display = "block";
-}
-function closeLogin() {
-    document.getElementById("loginBox").style.display = "none";
-}
