bb04f3156b7ab038052bb3f31543c833f16a8101
gmartini2019 <gmartini2019@my.fit.edu>
2023-04-03 22:46:11 -0400

    Firebase

---
+++ Milestone2/horsepower/src/Config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBXoZQDiXFibpQKnVgPhER7D6-ymW4uMM",
+  authDomain: "horsepower-4feee.firebaseapp.com",
+  projectId: "horsepower-4feee",
+  storageBucket: "horsepower-4feee.appspot.com",
+  messagingSenderId: "536036662694",
+  appId: "1:536036662694:web:9aa706d61a4cf186ce8367",
+  measurementId: "G-0LFXKJNFV0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
