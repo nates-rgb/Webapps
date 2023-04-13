6228c30491dc23ab6737b8fc8e80337971abf1d8
brycefowler141 <79876277+brycefowler141@users.noreply.github.com>
2023-04-12 22:46:23 -0400

    Create DB file
    Create DB file

---
+++ Milestone2/horsepower/src/pages/dB.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {buy1} from "./Buy"
+
+
+const carInventory = 
+[
+    {id:1, name: 'Car 1', brand:'Ferrari', model:'F40', color:'Red', description:'description of car', price:'2200000', boughtStatus: ''},
+    {id:2, name: 'Car 2', brand:'Aston Martin', model:'Valhalla', color:'Light Blue', description:'description of car', price:'2000000', boughtStatus: ''},
+    {id:3, name: 'Car 3', brand:'Ferrari', model:'LaFerrari', color:'Red', description:'description of car', price:'5000 Per Day', boughtStatus: ''},
+    {id:4, name: 'Car 4', brand:'Mercedes-Benz', model:'G Wagon', color:'White', description:'description of car', price:'2000 Per Day', boughtStatus: ''}
+];
+
+export {carInventory};
