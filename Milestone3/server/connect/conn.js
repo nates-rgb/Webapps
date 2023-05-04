296f9ec87f88d7dddfd99d4affbd033e381ee7eb
nate Hagen <nates50checy@gmail.com>
2023-05-03 21:53:04 -0400

    starting the express

---
+++ Milestone3/server/connect/conn.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _db;
+console.log("LetsGo"); 
+module.exports = {
+  connectToServer: function (callback) {
+    console.log("Layer3");	  
+    
+      // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    _db = client.db("horsepower")
+    if(_db){
+      console.log("obj exsist");
+      
+    
+    }
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
+
+
+
+// const client = new MongoClient('mongodb+srv://todoAppUser:<password>@cluster0.6lvjr.mongodb.net/myDatabase?retryWrites=true&w=majority')
+
+// async function start(){
+//   await client.connect()
+//   console.log("Connected")
+//   module.exports = client.db()
+//   const app = require('./app')
+//   app.listen(3000)
+// }
+
+//   start()
--- Milestone3/src/pages/Buy.js
+++ Milestone3/src/pages/Buy.js
@@ -6,8 +6,56 @@ import {Header} from "./universalComponets/Header"
 import {Nav} from "./universalComponets/Nav"
 import {Footer} from "./universalComponets/Footer"
 import {carInventory} from './dB'
+const Record = (props) => (
+    <tr>
+      <td>{props.record.name}</td>
+      <td>{props.record.position}</td>
+      <td>{props.record.level}</td>
+    
+    </tr>
+   );
 
 export function Buy() {
+
+    const [records, setRecords] = useState([]);
+ 
+    // This method fetches the records from the database.
+    useEffect(() => {
+      async function getRecords() {
+       console.log("hey i ran");
+        const response = await fetch(`http://localhost:5000/item/`);
+        console.log("i got past the fetch");
+        console.log(response);
+        
+        if (!response.ok) {
+          const message = `An error occurred: ${response.statusText}`;
+          window.alert(message);
+          return;
+        }
+    
+        const records = await response.json();
+        console.log(records)
+        setRecords(records);
+      }
+    
+      getRecords();
+    
+      return;
+    }, [records.length]);
+
+    // This method will map out the records on the table
+    function recordList() {
+        return records.map((record) => {
+        return (
+            <Record
+            record={record}
+            key={record._id}
+            />
+        );
+        });
+    }
+
+    
     //Car 1
     const [buy1, buyStatus1] = useState(() => {
         const storedBuyStatus1 = localStorage.getItem('buy1');
@@ -36,47 +84,62 @@ export function Buy() {
     if(localStorage.getItem("Authenticated")){
     return(
 
-        <div id = 'main_body'>
-            <Header />
-            <Nav />
-        <article>
-            <div className = "item1">
-                <div className = "row">
-                    <div className = "column left">
-                        <img src = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/e1-1674140223.jpeg" alt = "Car Buy Page" className = "buyPageImage"/>
-                    </div>
-                    <div className = "column middle">
-                        <h2>{carInventory[0].name}</h2>
-                        <h4>Brand: {carInventory[0].brand} &nbsp; Model: {carInventory[0].model} &nbsp; Color: {carInventory[0].color}</h4>
-                        <p>{carInventory[0].description}</p>
-                    </div>
-                    <div className = "column right">
-                        <h2>${carInventory[0].price}</h2>
-                        <button className = "button" onClick = {handleClick1}><img src = {buy_larger} alt ="buy now image1"/></button>
-                    </div>
-                </div>
-            </div>
-            <br/>
-            <div className = "item2">
-                <div className = "row">
-                    <div className = "column left">
-                        <img src = "https://hips.hearstapps.com/hmg-prod/images/aston-martin-valhalla-101-1626200852.jpg?crop=1.00xw:0.751xh;0,0.170xh&resize=1200:*" alt = "Car Buy Page" className = "buyPageImage"/>
-                    </div>
-                    <div className = "column middle">
-                        <h2>{carInventory[1].name}</h2>
-                        <h4>Brand: {carInventory[1].brand} &nbsp; Model: {carInventory[1].model} &nbsp; Color: {carInventory[1].color}</h4>
-                        <p>{carInventory[1].description}</p>
-                    </div>
-                    <div className = "column right">
-                        <h2>${carInventory[1].price}</h2>
-                        <button className = "button" onClick = {handleClick2}><img src = {buy_larger} alt ="buy now image1"/></button>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <Footer />
-    </div>       
-    )
+    //     <div id = 'main_body'>
+    //         <Header />
+    //         <Nav />
+    //     <article>
+    //         <div className = "item1">
+    //             <div className = "row">
+    //                 <div className = "column left">
+    //                     <img src = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/e1-1674140223.jpeg" alt = "Car Buy Page" className = "buyPageImage"/>
+    //                 </div>
+    //                 <div className = "column middle">
+    //                     <h2>{carInventory[0].name}</h2>
+    //                     <h4>Brand: {carInventory[0].brand} &nbsp; Model: {carInventory[0].model} &nbsp; Color: {carInventory[0].color}</h4>
+    //                     <p>{carInventory[0].description}</p>
+    //                 </div>
+    //                 <div className = "column right">
+    //                     <h2>${carInventory[0].price}</h2>
+    //                     <button className = "button" onClick = {handleClick1}><img src = {buy_larger} alt ="buy now image1"/></button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         <br/>
+    //         <div className = "item2">
+    //             <div className = "row">
+    //                 <div className = "column left">
+    //                     <img src = "https://hips.hearstapps.com/hmg-prod/images/aston-martin-valhalla-101-1626200852.jpg?crop=1.00xw:0.751xh;0,0.170xh&resize=1200:*" alt = "Car Buy Page" className = "buyPageImage"/>
+    //                 </div>
+    //                 <div className = "column middle">
+    //                     <h2>{carInventory[1].name}</h2>
+    //                     <h4>Brand: {carInventory[1].brand} &nbsp; Model: {carInventory[1].model} &nbsp; Color: {carInventory[1].color}</h4>
+    //                     <p>{carInventory[1].description}</p>
+    //                 </div>
+    //                 <div className = "column right">
+    //                     <h2>${carInventory[1].price}</h2>
+    //                     <button className = "button" onClick = {handleClick2}><img src = {buy_larger} alt ="buy now image1"/></button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </article>
+    //     <Footer />
+    // </div>       
+    // )
+    <div>
+     <h3>Record List</h3>
+     <table className="table table-striped" style={{ marginTop: 20 }}>
+       <thead>
+         <tr>
+           <th>Name</th>
+           <th>Position</th>
+           <th>Level</th>
+           <th>Action</th>
+         </tr>
+       </thead>
+       <tbody>{recordList()}</tbody>
+     </table>
+   </div>
+ );
     }
     return(<><Navigate replace to="/" /></>)
 }
