a4f7a756a58edfe25b864261e707af9d5ba4e9e7
brycefowler141 <brycefowler141@gmail.com>
2023-05-03 21:15:05 -0400

    header changes

--- Milestone3/src/pages/universalComponets/Header.js
+++ Milestone3/src/pages/universalComponets/Header.js
@@ -18,7 +18,7 @@ export function Header() {
         <div>
             <header id='logo'>
     
-                <Link to="/"><img src= {logo} alt = "Logo"/></Link>
+                <Link to="/"><img src= {logo} alt = "Logo" className = "headerImage"/></Link>
     
                 <div id='login_button'>
                 <Link to="/Login"><img src={login} alt = "login page button" /></Link>
